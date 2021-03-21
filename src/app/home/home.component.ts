f34c51bf890554a1b3c46a03f9bae90471b21aae
Jacob <jacob.saini60@gmail.com>
2021-03-21 10:40:18 +1000

    add express, change api url,

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { JudoService } from '../services/judo.service'
 
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -18,18 +19,19 @@ export class HomeComponent implements OnInit, OnDestroy {
   category:number = 0;
   name:string = '';
   judo:any;
-
+ 
 
   constructor(private judoService:JudoService) { }
 
   ngOnInit(){
+    
     this.judoService.getMoves(this.category,this.name).pipe(takeUntil(this.unsub)).subscribe(moves =>{
       console.log(moves)
       this.judo = moves
     })
   }
 
-  
+ 
 
   ngOnDestroy(){
     this.unsub.next()
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
+
+
+
 export const environment = {
   production: true,
-  API_URL:'http://localhost:8887'
+  API_URL:'https://judo.probacks.xyz'
+  
 };
